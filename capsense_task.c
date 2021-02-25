38fbddfcbac955d8c7cd0d1a40dd0263a6e10de1
gitlab-runner <do-not-reply@cypress.com>
2021-02-25 06:39:22 -0600

    Upload mtb-example-psoc6-capsense-buttons-slider-freertos 2.1.0.131

--- capsense_task.c
+++ capsense_task.c
@@ -5,23 +5,24 @@
 *
 * Related Document: README.md
 *
-********************************************************************************
-* (c) 2019-2020, Cypress Semiconductor Corporation. All rights reserved.
-********************************************************************************
-* This software, including source code, documentation and related materials
-* (“Software”), is owned by Cypress Semiconductor Corporation or one of its
-* subsidiaries (“Cypress”) and is protected by and subject to worldwide patent
-* protection (United States and foreign), United States copyright laws and
-* international treaty provisions. Therefore, you may use this Software only
-* as provided in the license agreement accompanying the software package from
-* which you obtained this Software (“EULA”).
+*******************************************************************************
+* Copyright 2019-2021, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
 *
-* If no EULA applies, Cypress hereby grants you a personal, nonexclusive,
-* non-transferable license to copy, modify, and compile the Software source
-* code solely for use in connection with Cypress’s integrated circuit products.
-* Any reproduction, modification, translation, compilation, or representation
-* of this Software except as specified above is prohibited without the express
-* written permission of Cypress.
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
 *
 * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
 * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
@@ -31,11 +32,11 @@
 * Software or any product or circuit described in the Software. Cypress does
 * not authorize its products for use in any products where a malfunction or
 * failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death (“High Risk Product”). By
-* including Cypress’s product in a High Risk Product, the manufacturer of such
-* system or application assumes all risk of such use and in doing so agrees to
-* indemnify Cypress against all liability.
-*****************************************​**************************************/
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
 
 
 /******************************************************************************
@@ -74,6 +75,7 @@ static void capsense_end_of_scan_callback(cy_stc_active_scan_sns_t* active_scan_
 static void capsense_timer_callback(TimerHandle_t xTimer);
 void handle_error(void);
 
+
 /******************************************************************************
 * Global variables
 ******************************************************************************/
@@ -100,6 +102,7 @@ cy_stc_syspm_callback_t capsense_deep_sleep_cb =
     NULL,
     NULL
 };
+
 
 /*******************************************************************************
 * Function Name: handle_error
@@ -209,6 +212,7 @@ void task_capsense(void* param)
         }
     }
 }
+
 
 /*******************************************************************************
 * Function Name: process_touch
@@ -381,7 +385,7 @@ static void capsense_end_of_scan_callback(cy_stc_active_scan_sns_t* active_scan_
 *******************************************************************************/
 static void capsense_timer_callback(TimerHandle_t xTimer)
 {
-	Cy_CapSense_Wakeup(&cy_capsense_context);
+    Cy_CapSense_Wakeup(&cy_capsense_context);
     capsense_command_t command = CAPSENSE_SCAN;
     BaseType_t xYieldRequired;
 
@@ -434,5 +438,6 @@ static void tuner_init(void)
     }
 
 }
+
 
 /* END OF FILE [] */
--- main.c
+++ main.c
@@ -12,23 +12,25 @@
 *
 * Related Document: See README.md
 *
-*******************************************************************************
-* (c) 2019-2020, Cypress Semiconductor Corporation. All rights reserved.
-*******************************************************************************
-* This software, including source code, documentation and related materials
-* ("Software"), is owned by Cypress Semiconductor Corporation or one of its
-* subsidiaries ("Cypress") and is protected by and subject to worldwide patent
-* protection (United States and foreign), United States copyright laws and
-* international treaty provisions. Therefore, you may use this Software only
-* as provided in the license agreement accompanying the software package from
-* which you obtained this Software ("EULA").
 *
+*******************************************************************************
+* Copyright 2019-2021, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+*
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
 * If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software source
-* code solely for use in connection with Cypress's integrated circuit products.
-* Any reproduction, modification, translation, compilation, or representation
-* of this Software except as specified above is prohibited without the express
-* written permission of Cypress.
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
 *
 * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
 * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
@@ -39,15 +41,10 @@
 * not authorize its products for use in any products where a malfunction or
 * failure of the Cypress product may reasonably be expected to result in
 * significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer of such
-* system or application assumes all risk of such use and in doing so agrees to
-* indemnify Cypress against all liability.
-*****************************************​**************************************/
-
-
-/******************************************************************************
-* Include header files
-******************************************************************************/
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
 
 #include "cybsp.h"
 #include "FreeRTOS.h"
@@ -63,15 +60,15 @@
 /* Priorities of user tasks in this project. configMAX_PRIORITIES is defined in
  * the FreeRTOSConfig.h and higher priority numbers denote high priority tasks.
  */
-#define TASK_CAPSENSE_PRIORITY      (configMAX_PRIORITIES - 1)
-#define TASK_LED_PRIORITY           (configMAX_PRIORITIES - 2)
+#define TASK_CAPSENSE_PRIORITY (configMAX_PRIORITIES - 1)
+#define TASK_LED_PRIORITY (configMAX_PRIORITIES - 2)
 
 /* Stack sizes of user tasks in this project */
-#define TASK_CAPSENSE_STACK_SIZE    (256u)
-#define TASK_LED_STACK_SIZE         (configMINIMAL_STACK_SIZE)
+#define TASK_CAPSENSE_STACK_SIZE (256u)
+#define TASK_LED_STACK_SIZE (configMINIMAL_STACK_SIZE)
 
 /* Queue lengths of message queues used in this project */
-#define SINGLE_ELEMENT_QUEUE        (1u)
+#define SINGLE_ELEMENT_QUEUE (1u)
 
 
 /*******************************************************************************
@@ -104,10 +101,10 @@ int main(void)
     /* Create the queues. See the respective data-types for details of queue
      * contents
      */
-    led_command_data_q  = xQueueCreate(SINGLE_ELEMENT_QUEUE,
-                                       sizeof(led_command_data_t));
-    capsense_command_q  = xQueueCreate(SINGLE_ELEMENT_QUEUE,
-                                       sizeof(capsense_command_t));
+    led_command_data_q = xQueueCreate(SINGLE_ELEMENT_QUEUE,
+                                      sizeof(led_command_data_t));
+    capsense_command_q = xQueueCreate(SINGLE_ELEMENT_QUEUE,
+                                      sizeof(capsense_command_t));
 
     /* Create the user tasks. See the respective task definition for more
      * details of these tasks.
@@ -120,15 +117,15 @@ int main(void)
     /* Start the RTOS scheduler. This function should never return */
     vTaskStartScheduler();
 
-    /*~~~~~~~~~~~~~~~~~~~~~ Should never get here! ~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    /********************** Should never get here ***************************/
     /* RTOS scheduler exited */
     /* Halt the CPU if scheduler exits */
     CY_ASSERT(0);
- 
-    for(;;)
-    {
 
+    for (;;)
+    {
     }
+
 }
 
 
